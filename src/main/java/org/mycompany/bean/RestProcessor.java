b08b6e6f67f04b68ff883685a6cf7c73d9ca4c21
Lim Li Khia <likhia@hotmail.com>
2020-09-01 18:19:50 +0800

    sixth commit

--- src/main/java/org/mycompany/bean/RestProcessor.java
+++ src/main/java/org/mycompany/bean/RestProcessor.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
-@Component(value = "restProcessor")
+@Component(value = "processor")
 @Configuration
 @RefreshScope
 public class RestProcessor implements Processor {
